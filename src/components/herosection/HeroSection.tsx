68c2662169ee3b8bdb85470eef2326221b59b12a
tahaamin2013 <tahaamindob2013@gmail.com>
2024-01-07 18:42:04 +0500

    fsdd

--- src/components/herosection/HeroSection.tsx
+++ src/components/herosection/HeroSection.tsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const HeroSection = () => {
   return (
-    <div>HeroSection</div>
+    <div className='bg-gradient-to-r from-blue-500 to-cyan-500 flex flex-col md:flex-row justify-between items-center px-6 w-full py-7 text-white'>
+        <div className='text-left'>
+        <h1>Love Humans Think Positive</h1>
+        <p>My aim Is Love No Poverty</p>
+        </div>
+        <div>
+            <div className='bg-white rounded-full p-24'>fs</div>
+        </div>  
+    </div>
   )
 }
 
