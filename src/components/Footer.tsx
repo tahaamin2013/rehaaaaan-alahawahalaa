e4f31d26a3de2f0dda1db318508b56ca6e4175c1
tahaamin2013 <tahaamindob2013@gmail.com>
2024-01-15 04:15:21 +0500

    ffasd

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const Footer = () => {
   return (
-    <div className='bg-[#2563eb] py-6 text-white flex flex-col text-center items-center'>
-d
+    <div className="bg-[#2563eb] py-6 text-white flex flex-col text-center items-center">
+      <div>© 2022 Rehan Allahwala. All Rights Reserved.</div>
+      <div className="text-xs flex flex-wrap text-center gap-0">
+        <Link href="sffsd">Who is Rehan Allahwala</Link>|
+        <Link href="sffsd">How To Teach</Link>|
+        <Link href="sffsd">Books</Link>|
+        <Link href="sffsd">500 Mutual Friend Experiment</Link>|
+        <Link href="sffsd">Contact</Link>|
+        <Link href="sffsd">Press</Link>|
+        <Link href="sffsd">Startup</Link>|
+        <Link href="sffsd">Ethics Series</Link>|
+        <Link href="sffsd">Canva group</Link>|
+        <Link href="sffsd">Chainak</Link>|
+        <Link href="sffsd">Abdullah Bot</Link>|
+        <Link href="sffsd">FBProfile</Link>|
+        <Link href="sffsd">LinkedIn Profile</Link>|
+        <Link href="sffsd">ChatGPT</Link>|
+        <Link href="sffsd">ABCD of Freeabcd</Link>|
+        <Link href="sffsd">Socials</Link>|
+        <Link href="sffsd">Meetup</Link>|
       </div>
- 
-    )
-}
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
